f227205204f2ef75a58e40aeabfa123ecece43b7
Eunice Musembi <jonathaneunice752@gmail.com>
2026-02-06 16:39:10 +0300

    login_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:graville_operations/screens/login/login_screen.dart';
-import 'package:graville_operations/screens/signup/signup_screen.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:graville_operations/main.dart';
 // ignore: depend_on_referenced_packages
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
