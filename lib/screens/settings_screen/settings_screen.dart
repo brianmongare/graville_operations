db4cc17f213c8d560dab35bf19d2e2d1aa62c734
laura-moenga <126756767+laura-001@users.noreply.github.com>
2026-02-12 14:17:50 +0300

    Update settings_screen.dart
    changed the accessors to public ,changed the buttons

--- lib/screens/settings_screen/settings_screen.dart
+++ lib/screens/settings_screen/settings_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:graville_operations/model/settings/settings_Item.dart';
+import 'package:graville_operations/screens/commons/widgets/section_Title.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
-  void _onItemTap(BuildContext context, String title) {
-    ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(SnackBar(content: Text('$title clicked')));
-  }
+  //void _onItemTap(BuildContext context, String title) {
+  //ScaffoldMessenger.of(
+  //context,
+  //).showSnackBar(SnackBar(content: Text('$title clicked')));
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +33,8 @@ class SettingsScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _sectionTitle('Account'),
+            //Account section
+            const SectionTitle(title: 'Account'),
             _settingsCard(
               context,
               items: [
@@ -44,7 +47,7 @@ class SettingsScreen extends StatelessWidget {
 
             const SizedBox(height: 20),
 
-            _sectionTitle('Preferences'),
+            const SectionTitle(title: 'Preferences'),
             _settingsCard(
               context,
               items: [
@@ -55,7 +58,7 @@ class SettingsScreen extends StatelessWidget {
 
             const SizedBox(height: 20),
 
-            _sectionTitle('Support & About'),
+            const SectionTitle(title: 'Support & About'),
             _settingsCard(
               context,
               items: [
@@ -66,7 +69,7 @@ class SettingsScreen extends StatelessWidget {
 
             const SizedBox(height: 20),
 
-            _sectionTitle('Actions'),
+            const SectionTitle(title: 'Actions'),
             _settingsCard(
               context,
               items: [
@@ -81,23 +84,23 @@ class SettingsScreen extends StatelessWidget {
     );
   }
 
-  Widget _sectionTitle(String title) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
-        ),
-      ),
-    );
-  }
+  //'Widget _sectionTitle(String title) {
+  //return Padding(
+  //padding: const EdgeInsets.only(bottom: 8),
+  //child: Text(
+  //title,
+  //style: const TextStyle(
+  //fontSize: 18,
+  //fontWeight: FontWeight.bold,
+  //color: Colors.black,
+  //),
+  //),
+  //);
+  //}
 
   Widget _settingsCard(
     BuildContext context, {
-    required List<_SettingsItem> items,
+    required List<SettingsItem> items,
   }) {
     return Container(
       decoration: BoxDecoration(
@@ -106,28 +109,44 @@ class SettingsScreen extends StatelessWidget {
       ),
       child: Column(
         children: items.map((item) {
-          return ListTile(
-            leading: Icon(item.icon, color: Colors.black54),
-            title: Text(
-              item.title,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          //return ListTile(
+          // leading: Icon(item.icon, color: Colors.black54),
+          //title: Text(
+          //item.title,
+          // style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          //),
+          //trailing: const Icon(Icons.chevron_right),
+          //onTap: () => _onItemTap(context, item.title),
+          //);
+
+          return Material(
+            color: Colors.transparent,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(16),
+              splashColor: Colors.black.withOpacity(0.05),
+              highlightColor: Colors.black.withOpacity(0.08),
+              onTap: () {
+                // TODO: navigate or handle action
+              },
+              child: ListTile(
+                leading: Icon(item.icon, color: Colors.black54),
+                title: Text(
+                  item.title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                trailing: const Icon(Icons.chevron_right),
+              ),
             ),
-            trailing: const Icon(Icons.chevron_right),
-            onTap: () => _onItemTap(context, item.title),
           );
         }).toList(),
       ),
     );
   }
 
-  _SettingsItem _item(IconData icon, String title) {
-    return _SettingsItem(icon: icon, title: title);
+  SettingsItem _item(IconData icon, String title) {
+    return SettingsItem(icon: icon, title: title);
   }
-}
-
-class _SettingsItem {
-  final IconData icon;
-  final String title;
-
-  _SettingsItem({required this.icon, required this.title});
 }
