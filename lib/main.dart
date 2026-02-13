dbf7a94ad264ab06e88ee3652737e26494e5e266
Eunice Musembi <jonathaneunice752@gmail.com>
2026-02-13 15:21:30 +0300

    updated login screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const SettingsScreen(),
+      home: const LoginScreen(),
 
 
     );
